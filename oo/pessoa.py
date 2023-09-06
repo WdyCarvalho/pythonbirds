5ceb35a36f1a11a40b0bce860141ab20e12bff63
WdyCarvalho <wdycarvalho@hotmail.com>
2023-09-06 10:57:33 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,8 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):# o primeiro parâmetro do método, por convenção é chamado 'self'
+        return 'olá!'
+
+if __name__ == '__main__':
+    p = Pessoa() #Variável P recebe a classe Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(p.cumprimentar( ))
