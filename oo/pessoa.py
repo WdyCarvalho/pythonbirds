0521546f8a3ab7312d47ea9a346f4161145a5de9
WdyCarvalho <wdycarvalho@hotmail.com>
2023-09-06 11:12:42 -0300

    Update pessoa.py
    Id dos termo 'self' e da variável 'p'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,9 @@
 class Pessoa:
     def cumprimentar(self):# o primeiro parâmetro do método, por convenção é chamado 'self'
-        return 'olá!'
+        return 'olá! {id(self)}'
 
 if __name__ == '__main__':
     p = Pessoa() #Variável P recebe a classe Pessoa()
     print(Pessoa.cumprimentar(p))
+    print(id(p))
     print(p.cumprimentar( ))
